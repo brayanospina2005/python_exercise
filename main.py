fe2e1397be9c6982cbf56f7abd254368deb40a6f
Brayan Ospina Marin <88215460+brayanospina2005@users.noreply.github.com>
2022-11-15 02:44:11 +0000

    dot and Cross

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import numpy as np
 
-a=np.array([1,2])
-b=np.array([3,4])
+#a=np.array([1,2])
+#b=np.array([3,4])
 
 # (1*3) + (2*4)
 
-resultado= np.dot(a,b)
-print(resultado)
+#resultado= np.dot(a,b)
+#print(resultado)
 
-print()
+#print()
 
-resultado=np.cross(a,b)
-print(resultado)
+#resultado=np.cross(a,b)
+#print(resultado)
+
+n= int(input())
+
+data= [list(map(int, input().split())) for i in range (n)]
+
+a= np.array(data)
+
+data= [list(map(int, input().split())) for i in range (n)]
+b= np.array(data)
+result=np.matmul(a,b)
+print(result)
